d74ebc04d1bbd8a38eb302ee9136e3c37eb1b2a7
zadzbw <609842385@qq.com>
2024-05-29 20:38:25 +0400

    feat: 🎸 support dark mode

--- unocss.config.ts
+++ unocss.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'unocss/vite'
 import { presetAttributify, presetIcons, presetUno, transformerDirectives } from 'unocss'
 
+// @ts-expect-error - can ignore it
+import { colors } from '@unocss/preset-uno/colors'
+
 export default defineConfig({
+  shortcuts: [{
+    'body-bg': 'bg-white @dark:bg-dark-400',
+    'primary-text': 'text-dark-400 @dark:text-light-300',
+  }],
+  theme: {
+    colors: {
+      aaa: colors.red['500'],
+    },
+  },
   presets: [
     presetUno(),
     presetAttributify(),
